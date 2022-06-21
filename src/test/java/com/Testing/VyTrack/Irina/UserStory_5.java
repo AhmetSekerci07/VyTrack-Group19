f4aa2aadb259d71e662dc95b2a4d0d792435f823
Irina <irinapiksaeva@yahoo.com>
2022-06-21 17:08:44 -0600

    Work in progress

--- src/test/java/com/Testing/VyTrack/Irina/UserStory_5.java
+++ src/test/java/com/Testing/VyTrack/Irina/UserStory_5.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 public class UserStory_5 extends TestBase {
 
 @Test
-    public void userStory5 (){
+    public void userStory5 () throws InterruptedException {
 
     driver.get("https://qa2.vytrack.com/user/login");
 
@@ -30,19 +30,36 @@ public class UserStory_5 extends TestBase {
 
 
 
-
-
     //1. truck driver see all cars' cost information on the Vehicle Costs page.
 
 //    WebElement setting = driver.findElement(By.xpath("//i[@class='fa-cog hide-text']"));
 //    setting.click();
+//
+//    WebElement selectAll = driver.findElement(By.xpath("//a[@class='pull-right']"));
+//    Thread.sleep(3000);
+//    selectAll.click();
+//    Thread.sleep(3000);
+//    System.out.println("selectAll.isSelected() = " + selectAll.isSelected());
+
 
 
 //    2. truck driver can create Vehicle cost.
+
+    WebElement createVehicleCostBtn = driver.findElement(By.xpath("//a[@class='btn main-group btn-primary pull-right ']"));
+    Thread.sleep(3000);
+    createVehicleCostBtn.click();
+    Thread.sleep(3000);
+
 //    3. truck driver can cancel Vehicle cost.
+
+    WebElement cancelBtn = driver.findElement(By.xpath("//a[@class='btn back icons-holder-text ']"));
+    cancelBtn.click();
+
 //    4. truck driver can edit Vehicle cost
 //    5. truck driver can delete Vehicle cost
+
 //    6. truck driver can add Attachment on General information page by click on any of the car on the grid.
+
 //    7. truck driver can reset the grid by click on Grid setting
 }
 
