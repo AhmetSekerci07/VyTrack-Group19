b2aa38d543eeca96c53d62d40bedcdad8d93be12
ZelihaKorkmaz <98862561+ZelihaKorkmaz@users.noreply.github.com>
2022-06-27 14:02:28 -0400

    Zeliha Version 1

--- src/test/java/com/Base/TestBase.java
+++ src/test/java/com/Base/TestBase.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import java.util.concurrent.TimeUnit;
+
 
 public class TestBase { public WebDriver driver;
 
@@ -14,7 +14,7 @@ public class TestBase { public WebDriver driver;
     public void setup(){
         driver = DriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
 
 
 
