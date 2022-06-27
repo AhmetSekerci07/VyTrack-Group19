089f79dbe591a622596a96cb4451e5762db9ab57
Irina <irinapiksaeva@yahoo.com>
2022-06-26 22:48:19 -0600

    Work in progress

--- src/test/java/com/Testing/VyTrack/Irina/UserStory_5.java
+++ src/test/java/com/Testing/VyTrack/Irina/UserStory_5.java
@@ -5,6 +5,8 @@ import com.Testing.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -64,24 +66,57 @@ public class UserStory_5 {
 
 //        4. truck driver can edit Vehicle cost
 
-        WebElement threeDots = Driver.getDriver().findElement(By.xpath("(//a[@data-toggle='dropdown'])[8]"));
+        WebElement threeDots = Driver.getDriver().findElement(By.xpath("//div[@class='dropdown']/a[1]"));
         System.out.println("threeDots.isDisplayed() = " + threeDots.isDisplayed());
+        Thread.sleep(3000);
 
-        Actions actions = new Actions(Driver.getDriver());
-        actions.moveToElement(threeDots).perform();
+        Actions actionDots = new Actions(Driver.getDriver());
+        actionDots.moveToElement(threeDots).perform();
 
-        WebElement editBtn = Driver.getDriver().findElement(By.xpath("(//a[@title='Edit'])[3]"));
+        WebElement editBtn = Driver.getDriver().findElement(By.xpath("(//ul[@class='nav nav-pills icons-holder launchers-list'])[1]//li[2]"));
         System.out.println("editBtn.isDisplayed() = " + editBtn.isDisplayed());
+
+        Actions actionEdit = new Actions(Driver.getDriver());
+        Thread.sleep(3000);
+        actionEdit.moveToElement(threeDots).perform();
+        editBtn.click();
+        Thread.sleep(2000);
 
 
 //        5. truck driver can delete Vehicle cost
 
-        //WebElement deleteBtn = Driver.getDriver().findElement(By.xpath("(//a[@title='Delete'])[3]"));
-
-        //List<WebElement> dots = Driver.getDriver().findElement(By.xpath("(//a[@data-toggle='dropdown'])[8]"));
+//        Actions actionDots2 = new Actions(Driver.getDriver());
+//        actionDots2.moveToElement(threeDots).perform();
+//
+//        WebElement deleteBtn = Driver.getDriver().findElement(By.xpath("(//ul[@class='nav nav-pills icons-holder launchers-list'])[1]/li[3]"));
+//
+//        System.out.println("deleteIcon.isDisplayed() = " + deleteBtn.isDisplayed());
+//
+//        Actions actionDelete = new Actions(Driver.getDriver());
+//        actionDelete.moveToElement(deleteBtn).perform();
+//        Thread.sleep(3000);
+//        deleteBtn.click();
 
 //        6.  truck driver can add Attachment on General information by click on any of the car on the grid.
+
+//        WebElement anyCar = Driver.getDriver().findElement(By.xpath("(//tr[@class='grid-row row-click-action'])[1]"));
+//        anyCar.click();
+//        Thread.sleep(3000);
+//
+//        WebElement attachBtn = Driver.getDriver().findElement(By.xpath("//a[@title='Add attachment']"));
+//        attachBtn.click();
+//        Thread.sleep(3000);
+//
+//        WebElement chooseBtn = Driver.getDriver().findElement(By.xpath("//span[@class='action']"));
+//        Thread.sleep(3000);
+//
+//        String path="C:\\Users\\irina\\OneDrive\\Desktop\\some-file.txt";
+//        chooseBtn.sendKeys(path);
+
 //        7. truck driver can reset the grid by click on Grid setting
+
+        WebElement resetBtn = Driver.getDriver().findElement(By.xpath("//a[@title='Reset']"));
+        resetBtn.click();
 
         Driver.getDriver().quit();
     }
